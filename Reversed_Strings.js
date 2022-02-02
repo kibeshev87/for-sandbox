a4d0fec8cc1037e39b0e22db76ce8a46fc624d9d
kibeshev87 <87781931+kibeshev87@users.noreply.github.com>
2022-02-02 08:55:00 +0200

    Add files via upload

---
+++ Reversed_Strings.js
@@ -0,0 +1,7 @@
+function solution(str){
+    return str.split("").reverse().join("");
+}
+
+/*split -- разбивает строку на символы;
+reverse -- переворачивает массив;
+join -- собирает строку из массива*/
